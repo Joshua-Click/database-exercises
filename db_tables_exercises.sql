74202f6447b1fa7c84b04ed211bf7d44b2955e4f
Joshua-Click <joshua.click25@gmail.com>
2023-07-06 11:16:56 -0500

    fixed my errors

--- db_tables_exercises.sql
+++ db_tables_exercises.sql
@@ -1,18 +1,23 @@
 -- List all databases
 SHOW DATABASES;
 -- Use albums database
-SELECT albums_db;
+USE albums_db;
+
 -- Show the currently selected database
 SELECT database();
+
 -- List all tables in the database
 SHOW TABLES;
 
 -- switch to the employees database
 USE employees;
+
 -- how the currently selected database
 SELECT database();
+
 -- List all tables in the database
 SHOW TABLES;
+
 -- What dif data types are present in this table?
 SELECT * FROM employees;
 DESCRIBE employees;
@@ -27,7 +32,9 @@ DESCRIBE employees;
 -- What is the relationship between the employees and the departments tables?
 SELECT * FROM departments;
 DESCRIBE departments;
--- The employee table and the department tables have nothing in common
+-- The employee table and the department tables have no direct link
+-- But combined they are linked by where the employees work in each dept
+
 -- SQL code that created the dept_manager table
 SHOW CREATE TABLE dept_manager;
 
