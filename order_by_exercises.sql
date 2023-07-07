077396304527fb7841fcc83efc755cda7c9b4e3c
Joshua-Click <joshua.click25@gmail.com>
2023-07-07 09:31:33 -0500

    fixed my errors

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,12 +4,14 @@ USE employees;
 -- In your comments, answer: What was the first and last name in the first row of the results? 
 -- What was the first and last name of the last person in the table?
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
--- First: Irena Reutenauer / Last: Vidya Simment
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name limit 1 offset 708;
+-- First: Irena Reutenauer / Last: Vidya Simmen
 
 -- Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name and then last name. 
 -- In your comments, answer: What was the first and last name in the first row of the results? 
 -- What was the first and last name of the last person in the table?
-SELECT * FROM employees WHERE first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya' ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name limit 1 offset 708;
 -- First: Irena Acton / Last: Vidya Zweizig
 
 -- Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name.  
@@ -24,6 +26,11 @@ SELECT * FROM employees WHERE (first_name = 'Irena' or first_name = 'Vidya' or f
 SELECT * FROM employees 
 WHERE last_name LIKE 'e%e'
 ORDER BY emp_no;
+
+SELECT * FROM employees 
+WHERE last_name LIKE 'e%e'
+ORDER BY emp_no limit 1 offset 898;
+
 -- Number of Emps Returned = 899 / 10021 Ramzi Erde / 499648 Tadahiro Erde
 
 -- Write a query to find all employees whose last name starts and ends with 'E'. 
@@ -38,7 +45,11 @@ ORDER BY hire_date DESC;
 -- Find all employees hired in the 90s and born on Christmas. 
 -- Sort the results so that the oldest employee who was hired last is the first result. 
 -- Enter a comment with the number of employees returned, the name of the oldest employee who was hired last, and the name of the youngest employee who was hired first.
-SELECT * FROM employees WHERE birth_date like '%-12-25' AND hire_date between '1990-01-01' AND '1999-12-31' 
+SELECT * FROM employees WHERE birth_date like '%-12-25' AND hire_date 
+between '1990-01-01' AND '1999-12-31' 
 ORDER BY birth_date, hire_date Desc;
+
+
+
 -- 362 Returned / Oldest&Last: Khun Bernini / Youngest&First: Douadi Pettis
 
