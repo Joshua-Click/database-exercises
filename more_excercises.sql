9c8cdb567929300d379a0104f314063a56b56ee5
Joshua-Click <joshua.click25@gmail.com>
2023-07-09 14:23:13 -0500

    just more practice

--- more_excercises.sql
+++ more_excercises.sql
@@ -19,18 +19,31 @@ SELECT DISTINCT postal_code FROM address;
 SELECT DISTINCT rating FROM film;
 
 -- WHERE clause
+SELECT * FROM film;
 -- Select the title, description, rating, movie length columns from the films table that last 3 hours or longer
-
+SELECT title, description, rating, length FROM film
+WHERE length > 180;
 -- Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005
-
+SELECT * FROM payment;
+SELECT payment_id, amount, payment_date FROM payment
+WHERE payment_date >= '2005-05-27';
 -- Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005
-
+DESCRIBE payment;
+SELECT payment_id, amount, payment_date FROM payment
+WHERE payment_date = '2005-05-27';
 -- Select all columns from the customer table for rows that have a last names beginning with S and a first names ending with N
-
+SELECT * FROM customer;
+SELECT * FROM customer
+WHERE last_name LIKE 'S%' AND first_name LIKE '%N';
 -- Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M"
+SELECT * FROM customer
+WHERE active = '0' AND last_name LIKE 'M%';
 
 -- Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either C, S or T
-
+DESCRIBE customer;
+SELECT * FROM customer
+WHERE customer_id > 4
+AND last_name LIKE 'c%' OR last_name LIKE 'S%' OR last_name LIKE 'T%';
 -- Select all columns minus the password column from the staff table for rows that contain a password
 
 -- Select all columns minus the password column from the staff table for rows that do not contain a password
