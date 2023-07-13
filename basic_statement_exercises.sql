a8a1aa63f7bbbef7d3383516f102d658c550ba64
Joshua-Click <joshua.click25@gmail.com>
2023-07-13 09:51:09 -0500

    specified answers

--- basic_statement_exercises.sql
+++ basic_statement_exercises.sql
@@ -13,9 +13,11 @@ SELECT * FROM albums;
 
 -- Find the name of all albums by Pink Floyd
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
+-- dark side of the moon, the wall
 
 -- What is the year Sgt. Pepper's Lonely Hearts Club Band was released?
 SELECT release_date FROM albums WHERE name = "Sgt. Pepper's Lonely Hearts Club Band";
+-- 1967
 
 -- What is the genre for the album Nevermind?
 SELECT genre FROM albums WHERE name = "Nevermind";
@@ -23,6 +25,8 @@ SELECT genre FROM albums WHERE name = "Nevermind";
 
 -- Which albums were released in the 1990s?
 SELECT * FROM albums WHERE release_date >= 1990 AND release_date <= 1999;
+-- 11 rows returned
 
 -- Which albums had less than 20M certified sales? Rename this column as low_selling_albums
-SELECT name as low_selling_albums FROM albums WHERE sales < 20;
+SELECT name as low_selling_albums, sales FROM albums WHERE sales < 20;
+-- 13 rows returned
